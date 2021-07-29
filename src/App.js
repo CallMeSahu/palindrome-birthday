64934c7b6b1a5fdc1a3b4f817af4b05cbd6aa9d7
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2021-07-29 23:45:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="about-area">
+        <h1>Palindrome Birthday</h1>
+        <h3>Find if your birthday is a palindrome or not.</h3>
+      </div>
+      <div className="working-area"></div>
+    </div>
+  );
+}
