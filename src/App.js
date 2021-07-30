4fcb4abc41cdc9ea6e85099d136c8fb19d519428
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2021-07-31 00:35:05 +0530

    style: basic components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import React from "react";
 
 export default function App() {
   return (
@@ -9,11 +10,16 @@ export default function App() {
           A palindrome is a word, number, phrase, or other sequence of
           characters which reads the same backward as forward.
         </p>
-        <button>
-          <a href="#workingSection" /> Find Out
-        </button>
+        <p>
+          This app will take your birthday as an input and check whether it is a
+          palindrome or not.
+        </p>
+        <p>It will also find out, by how many days you missed!</p>
       </section>
-      <section className="working-section" id="workingSection"></section>
+      <section className="working-section">
+        <h3>Enter Your Birthday</h3>
+        <input type="date"></input>
+      </section>
     </div>
   );
 }
