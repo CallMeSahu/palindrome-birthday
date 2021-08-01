f6ff15c5a538963beefee52a7d03f783cae7c2af
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2021-08-01 23:20:16 +0530

    feat: isPalindrome & checkComni functions

--- src/App.js
+++ src/App.js
@@ -5,17 +5,54 @@ import loading from "/src/loading.gif";
 export default function App() {
   const [output, setOutput] = useState("");
 
+  function isPalindrome(dateString) {
+    var dateStringReverse = "";
+    for (var i = dateString.length - 1; i >= 0; i--) {
+      dateStringReverse += dateString[i];
+    }
+    if (dateStringReverse === dateString) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function checkCombi(dd, mm, yyyy) {
+    var type1 = dd + mm + yyyy;
+    var type2 = mm + dd + yyyy;
+    var type3 = mm + dd + yyyy.slice(-2);
+
+    if (isPalindrome(type1) || isPalindrome(type2) || isPalindrome(type3)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function getNextDate(date, month, year) {}
+
+  function findResult(date) {
+    var arr = date.split("-");
+    var inputYear = arr[0];
+    var inputMonth = arr[1];
+    var inputDate = arr[2];
+
+    var flag = checkCombi(inputDate, inputMonth, inputYear);
+
+    if (flag === true) {
+      setOutput("Your birthday is a Palindrome 🙌🎉");
+    } else {
+      var [nextDate, days] = getNextDate(inputDate, inputMonth, inputYear);
+      var falseOutput = "Your bithday is not a Palindrome 🥺👉👈";
+      setOutput(falseOutput);
+    }
+  }
+
   function userInputHandler(event) {
     var date = event.target.value;
-    setOutput(
-      <img
-        style={{ width: "200px", margin: "1rem" }}
-        src={loading}
-        alt=""
-      ></img>
-    );
+    setOutput(<img style={{ width: "200px" }} src={loading} alt=""></img>);
     setTimeout(() => {
-      setOutput(date);
+      findResult(date);
     }, 7000);
   }
 
@@ -31,12 +68,16 @@ export default function App() {
           This app will take your birthday as an input and check whether it is a
           palindrome or not.
         </p>
+        <p>
+          This app will check your birthday in MM/DD/YYYY, DD/MM/YYYY and
+          MM/DD/YY formats.
+        </p>
         <p>It will also find out, by how many days you missed!</p>
       </section>
       <section className="working-section">
         <h3>Enter Your Birthday</h3>
         <input type="date" onChange={userInputHandler}></input>
-        <div>{output}</div>
+        <div style={{ padding: "1rem", fontWeight: "bold" }}>{output}</div>
       </section>
     </div>
   );
