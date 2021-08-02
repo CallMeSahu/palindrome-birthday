57dccb0b53c1a23793cab998aa238fce49288128
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2021-08-02 14:03:25 +0530

    feat: getNearestDate()

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./styles.css";
 import React, { useState } from "react";
 import loading from "/src/loading.gif";
 
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 export default function App() {
   const [output, setOutput] = useState("");
 
@@ -22,14 +24,81 @@ export default function App() {
     var type2 = mm + dd + yyyy;
     var type3 = mm + dd + yyyy.slice(-2);
 
-    if (isPalindrome(type1) || isPalindrome(type2) || isPalindrome(type3)) {
-      return true;
+    if (isPalindrome(type1)) {
+      return `${dd}-${mm}-${yyyy}`;
+    } else if (isPalindrome(type2)) {
+      return `${mm}-${dd}-${yyyy}`;
+    } else if (isPalindrome(type3)) {
+      return `${mm}-${dd}-${yyyy.slice(-2)}`;
     } else {
-      return false;
+      return null;
     }
   }
 
-  function getNextDate(date, month, year) {}
+  function getNearestDate(date, month, year) {
+    var dd1 = Number(date);
+    var mm1 = Number(month);
+    var yy1 = Number(year);
+    var dd2 = Number(date);
+    var mm2 = Number(month);
+    var yy2 = Number(year);
+
+    for (var i = 1; i > 0; i++) {
+      dd1++;
+      if (dd1 > Number(daysInMonth[mm1 - 1])) {
+        dd1 = 1;
+        mm1++;
+        if (mm1 > 12) {
+          mm1 = 1;
+          yy1++;
+        }
+      }
+
+      var ddString = dd1.toString();
+      var mmString = mm1.toString();
+      var yyString = yy1.toString();
+
+      if (ddString.length === 1) {
+        ddString = "0" + ddString;
+      }
+      if (mmString.length === 1) {
+        mmString = "0" + mmString;
+      }
+
+      var nextCorrectCombi = checkCombi(ddString, mmString, yyString);
+      if (nextCorrectCombi) {
+        return [`${nextCorrectCombi}`, i];
+      }
+
+      if (yy2 > 1) {
+        dd2--;
+        if (dd2 < 1) {
+          mm2--;
+          if (mm2 < 1) {
+            mm2 = 12;
+            yy2--;
+            if (yy2 < 1) {
+              break;
+            }
+            dd2 = daysInMonth[mm2 - 1];
+          }
+        }
+        var yyString = yy2.toString();
+        var mmString = mm2.toString();
+        var ddString = dd2.toString();
+        if (ddString.length === 1) {
+          ddString = "0" + ddString;
+        }
+        if (mmString.length === 1) {
+          mmString = "0" + mmString;
+        }
+        var prevCorrectCombi = checkCombi(ddString, mmString, yyString);
+        if (prevCorrectCombi) {
+          return [`${prevCorrectCombi}`, i];
+        }
+      }
+    }
+  }
 
   function findResult(date) {
     var arr = date.split("-");
@@ -37,20 +106,24 @@ export default function App() {
     var inputMonth = arr[1];
     var inputDate = arr[2];
 
-    var flag = checkCombi(inputDate, inputMonth, inputYear);
+    var correctCombi = checkCombi(inputDate, inputMonth, inputYear);
 
-    if (flag === true) {
-      setOutput("Your birthday is a Palindrome 🙌🎉");
+    if (correctCombi) {
+      setOutput(`Your birthday is a Palindrome in ${correctCombi} format!🙌🎉`);
     } else {
-      var [nextDate, days] = getNextDate(inputDate, inputMonth, inputYear);
-      var falseOutput = "Your bithday is not a Palindrome 🥺👉👈";
+      var [nearestDate, daysGap] = getNearestDate(
+        inputDate,
+        inputMonth,
+        inputYear
+      );
+      var falseOutput = `Your birthday is not a Palindrome. Nearest Palindrome date to your birthday was ${nearestDate} and you missed it by ${daysGap} days.🥺👉👈`;
       setOutput(falseOutput);
     }
   }
 
   function userInputHandler(event) {
     var date = event.target.value;
-    setOutput(<img style={{ width: "200px" }} src={loading} alt=""></img>);
+    setOutput(<img style={{ width: "250px" }} src={loading} alt=""></img>);
     setTimeout(() => {
       findResult(date);
     }, 7000);
@@ -77,7 +150,16 @@ export default function App() {
       <section className="working-section">
         <h3>Enter Your Birthday</h3>
         <input type="date" onChange={userInputHandler}></input>
-        <div style={{ padding: "1rem", fontWeight: "bold" }}>{output}</div>
+        <div
+          style={{
+            padding: "3rem",
+            fontWeight: "bold",
+            width: "500px",
+            margin: "auto"
+          }}
+        >
+          {output}
+        </div>
       </section>
     </div>
   );
